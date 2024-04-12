9864f9ad7989b980b6c4c40eb0edde9fbaecd2d2
Ellie Wright <elliewright055@gmail.com>
2024-04-12 08:14:00 -0500

    add header

--- src/app/page.js
+++ src/app/page.js
@@ -100,6 +100,7 @@ export default function Game() {
 
   return (
     <div className="game">
+      <h1>Tic-Tac-Toe</h1>
       <div className="game-board">
         <Board squares={currentSquares} xIsNext={xIsNext} onPlay={handlePlay} />
       </div>
